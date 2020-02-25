cb3c3034ae01b868246b2cc8d7386d4cae856270
liang10339 <34334123+liang10339@users.noreply.github.com>
2020-02-25 16:49:32 +0800

    add correct path

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
-import { About, Resume, Porrtfolio } from './pages';
+import { About, Resume, Porrtfolio, Portfolio } from './pages';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <Route path="/about" exact component={About} />
+        <Route path="/resume" exact component={Resume} />
+        <Route path="/portfolio" exact component={Portfolio} />
 
       </div>
     );
