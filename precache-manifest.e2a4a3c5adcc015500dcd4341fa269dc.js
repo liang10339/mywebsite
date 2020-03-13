48fff153f02209235a73e8cf622da3fdefc80cea
liang10339 <34334123+liang10339@users.noreply.github.com>
2020-03-13 20:21:13 +0800

    Updates

---
+++ precache-manifest.e2a4a3c5adcc015500dcd4341fa269dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c00372246f8eee826def0e79ff15326",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "46a2b784f92b4018b89f",
+    "url": "/mywebsite/static/css/main.25d1f4e0.chunk.css"
+  },
+  {
+    "revision": "dd960a638e9a245d3558",
+    "url": "/mywebsite/static/js/2.59e94602.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/mywebsite/static/js/2.59e94602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a2b784f92b4018b89f",
+    "url": "/mywebsite/static/js/main.56c668e7.chunk.js"
+  },
+  {
+    "revision": "8d73b069206632d8f8cc",
+    "url": "/mywebsite/static/js/runtime-main.94311097.js"
+  },
+  {
+    "revision": "767470765b9a809fb47546d48c7dfc79",
+    "url": "/mywebsite/static/media/logo_nanya_pic.76747076.svg"
+  },
+  {
+    "revision": "69c6d0a6954d30c1d2c77e5050feabdc",
+    "url": "/mywebsite/static/media/photo.69c6d0a6.JPG"
+  }
+]);
